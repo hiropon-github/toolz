18ef51114f43eccf65680eb55e7ae57164350c4e
hiropon-github <hiropon0628@outlook.com>
2019-12-01 23:43:12 +0900

    新規追加

---
+++ HrkMessage/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace HrkMessage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 371);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(713, 79);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(718, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "送信";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1, 1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(797, 364);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HrkMessage - S-HIROKI-EV Ver1.00";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ HrkMessage/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Timers;
+using System.Configuration;
+
+namespace HrkMessage
+{
+    public partial class Form1 : Form
+    {
+        public int fileCnt = 0;
+        //public const string CURRENT_DIRECTORY = Environment.CurrentDirectory;
+        //public const string CURRENT_DIRECTORY = @"\\koidrive\Files\03.業務共通\02.マーケティングIT企画本部\09.プロジェクトドキュメント\1000.福利厚生\H30年度\0011.サポート・ライセンス切れー.net1.1対応\90.その他\90.個人\廣木\HrkMessage";
+        public string CURRENT_DIRECTORY = ConfigurationManager.AppSettings["CurrentPath"];
+        private System.Timers.Timer timer = new System.Timers.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReadLogFile();
+            this.timer.Elapsed += new System.Timers.ElapsedEventHandler(OnElapsed_TimersTimer);
+            this.timer.Interval = 1000;
+            this.timer.Start();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textBox1.Text))
+            {
+                return;
+            }
+
+            PutTextSaveFile();
+        }
+
+        // KeyDown イベントのイベントハンドラ
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Alt+Enter キーが押されていたら
+            if (e.Alt && e.KeyCode == Keys.Enter)
+            {
+                if (string.IsNullOrEmpty(textBox1.Text))
+                {
+                    return;
+                }
+
+                PutTextSaveFile();
+            }
+        }
+
+        // TextBoxに入力された文字列をリッチテキストに出力して、ファイルに保存する処理
+        private void PutTextSaveFile()
+        {
+            Font baseFont = richTextBox1.SelectionFont;
+            Font fnt = new Font(baseFont.FontFamily, baseFont.Size, baseFont.Style | FontStyle.Bold);
+
+            this.richTextBox1.SelectionFont = fnt;
+            this.richTextBox1.SelectedText += Environment.MachineName + " - " + DateTime.Now + "--------------------" + Environment.NewLine;
+
+            this.richTextBox1.SelectionFont = baseFont;
+            this.richTextBox1.SelectedText += textBox1.Text + Environment.NewLine;
+
+            string str = Environment.MachineName + " - " + DateTime.Now + "--------------------" + Environment.NewLine;
+            str += textBox1.Text + Environment.NewLine;
+            string filePath = CURRENT_DIRECTORY + @"\msglog\";
+            string fileName = Environment.MachineName + "_" + DateTime.Now.ToString("yyyyMMddhhmmss") + ".txt";
+
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            File.WriteAllText(filePath + fileName, str);
+            fileCnt++;
+            textBox1.Clear();
+        }
+
+        private void ReadLogFile()
+        {
+            String filePath = CURRENT_DIRECTORY + @"\msglog\";
+            if (!Directory.Exists(filePath))
+            {
+                return;
+            }
+            //起動時は全ファイル表示
+            if(fileCnt == 0)
+            {
+                var files = Directory.GetFiles(filePath).OrderBy(f => File.GetCreationTime(f));
+                fileCnt = files.Count();
+                string readText;
+                foreach (var f in files)
+                {
+                    readText = File.ReadAllText(f);
+                    this.richTextBox1.SelectedText += readText;
+                }
+                this.richTextBox1.ScrollToCaret();
+            }
+            else
+            {
+                var files = Directory.GetFiles(filePath).OrderBy(f => File.GetCreationTime(f)).ToArray();
+                int cnt = files.Count();
+                if(fileCnt != cnt)
+                {
+                    var dst = new string[cnt - fileCnt];
+                    Array.Copy(files, fileCnt, dst, 0, cnt - fileCnt);
+                    string readText;
+                    foreach (var f in dst)
+                    {
+                        readText = File.ReadAllText(f);
+                        this.richTextBox1.SelectedText += readText;
+                    }
+                    this.richTextBox1.ScrollToCaret();
+                    fileCnt = cnt;
+                }
+                return;
+            }
+            
+        }
+        private async void OnElapsed_TimersTimer(object sender, ElapsedEventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                ReadLogFile();
+            });
+        }
+
+        private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+    }
+}
